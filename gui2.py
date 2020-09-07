e0141eb215f55b9ac9010f11837af1c3142082e9
Pawlo <pawlonazaruk@gmail.com>
2020-09-08 00:25:53 +0200

    class MyApp fixes

--- gui2.py
+++ gui2.py
@@ -2,76 +2,70 @@ from tkinter import *
 from main import *
 import json
 
-class MyApp:
+# TODO: sortowanie przy zapisie/wczytaniu pliku
 
-    def __init__(self, parent, tags):
-        self.myParent = parent
+class MyApp:
+    def __init__(self, root, tags):
+        self.root = root
         self.tags = tags
 
-        print("asd")
+        self.btn_new_tag = Button(self.root, text="New Tag", bg="green")
+        self.btn_new_tag.bind("<Button-1>", lambda event: NewTagWindow(self.root, "New Tag"))
+        self.btn_new_tag.pack(side=TOP, fill=X)
+        self.frm_content = Frame(self.root, height=20, borderwidth=5, bg="black")
+        self.frm_content.pack(fill=X)
 
-        self.button_NewTag = Button(self.myParent, text="New Tag", bg="green")
-        self.button_NewTag.bind("<Button-1>", lambda event: NewTagWindow(self.myParent, "New Tag"))
-        self.button_NewTag.pack(side=TOP, fill=X)
+        self.show_content()
 
-        self.myContainer1 = Frame(self.myParent, height=20, borderwidth=5, bg="black")
-        self.myContainer1.pack(fill=X)
-
-        self.showContent(tags)
-
-    def showContent(self, tags):
+    def show_content(self):
         try:
-            self.myScrollable_frame.destroy()
+            self.frm_content_scroll.destroy()
         except AttributeError:
             pass
         finally:
-            self.myScrollable_frame = ScrollableFrame(self.myContainer1)
+            self.frm_content_scroll = ScrollableFrame(self.frm_content)
 
-            for tag in tags:
-                print(tag['name'])
-                self.myContainer_tag = Frame(self.myScrollable_frame.scrollable_frame)
-                self.myContainer_tag.pack(fill=X)
+            for tag in self.tags:
+                self.frm_tag = Frame(self.frm_content_scroll.scrollable_frame)
+                self.frm_tag.pack(fill=X)
 
-                self.button_UpdateTag = Button(self.myContainer_tag, text="Update Tag", bg="yellow")
-                self.button_UpdateTag.bind("<Button-1>", \
-                                           lambda event: UpdateTagWindow(self.getCurrentTagDict(event, tags), self.myParent, "Update Tag"))
-                self.button_UpdateTag.pack(side=LEFT)
+                self.btn_update_tag = Button(self.frm_tag, text="Update Tag", bg="yellow")
+                self.btn_update_tag.bind("<Button-1>", lambda event:
+                                            UpdateTagWindow(self.get_current_tag(event, ""), self.root, "Update Tag"))
+                self.btn_update_tag.pack(side=LEFT)
 
-                self.button_DeleteTag = Button(self.myContainer_tag, text="Delete Tag", bg="red")
-                self.button_DeleteTag.bind("<Button-1>", lambda event: self.buttonDeleteClick(event, tags, self.getCurrentTagDict(event, tags, "2")))
-                self.button_DeleteTag.pack(side=LEFT)
+                self.btn_delete_tag = Button(self.frm_tag, text="Delete Tag", bg="red")
+                self.btn_delete_tag.bind("<Button-1>", lambda event:
+                                            self.btn_delete_tag_click(event, self.get_current_tag(event, "2")))
+                self.btn_delete_tag.pack(side=LEFT)
 
-                self.label_Name = Label(self.myContainer_tag, text="Name: ", bg="cyan")
-                self.label_Name.pack(side=LEFT)
+                self.lbl_name = Label(self.frm_tag, text="Name: ", bg="cyan")
+                self.lbl_name.pack(side=LEFT)
+                self.lbl_name_tag_value = Label(self.frm_tag, text=tag['name'], bg="white", width=18, anchor=W)
+                self.lbl_name_tag_value.pack(side=LEFT)
 
-                self.label_Name = Label(self.myContainer_tag, text=tag['name'], bg="white", width=18, anchor=W)
-                self.label_Name.pack(side=LEFT)
+                self.lbl_switch_to = Label(self.frm_tag, text="Switch_to", bg="cyan")
+                self.lbl_switch_to.pack(side=LEFT)
+                self.lbl_switch_to_tag_value = Label(self.frm_tag, text=tag['switch_to'], bg="white")
+                self.lbl_switch_to_tag_value.pack(side=LEFT)
 
-                self.label_Switch_to = Label(self.myContainer_tag, text="Switch_to", bg="cyan")
-                self.label_Switch_to.pack(side=LEFT)
+            self.frm_content_scroll.pack(fill=X)
 
-                self.label_Switch_to = Label(self.myContainer_tag, text=tag['switch_to'], bg="white")
-                self.label_Switch_to.pack(side=LEFT)
-
-            self.myScrollable_frame.pack(fill=X)
-
-
-
-    def getCurrentTagDict(self, event, tags, button_idx = ""):
+    def get_current_tag(self, event, button_idx = ""):
+        # TODO MAKE SIMPLER / regex??
         try:
-            self.idx = int(str(event.widget).replace(".!button" + button_idx, "")[46:]) - 1
+            self.tag_idx = int(str(event.widget).replace(".!button" + button_idx, "")[46:]) - 1
         except ValueError:
-            self.idx = 1 - 1
-        self.myDict = tags[self.idx]
-        return self.myDict
+            self.tag_idx = 0
+        return self.tags[self.tag_idx]
 
-    def buttonDeleteClick(self, event, tags, myDict):
+    def btn_delete_tag_click(self, event, myDict):
         report_event(event)
-        delete_tag(tags, myDict)
-        AlertWindow(self.myParent, "Success", "Tag was deleted.")
-        self.showContent(tags)
+        delete_tag(self.tags, myDict)
+        AlertWindow(self.root, "Success", "Tag was deleted.")
+        self.show_content()
 
-
+# //////////////////////////////////////////////////////////
 
 class UpdateTagWindow(Toplevel):
     def __init__(self, myDict, master=None, title="Update Tag Window"):
@@ -94,14 +88,14 @@ class UpdateTagWindow(Toplevel):
         self.text_Switch_to.insert("1.0", self.myDict["switch_to"])
         self.text_Switch_to.pack(fill=X)
 
-        self.myContainer1 = Frame(self)
-        self.myContainer1.pack()
+        self.frm_container_01 = Frame(self)
+        self.frm_container_01.pack()
 
-        self.button_Create = Button(self.myContainer1, text="Update", bg="yellow")
+        self.button_Create = Button(self.frm_container_01, text="Update", bg="yellow")
         self.button_Create.bind("<Button-1>", self.buttonUpdateClick)
         self.button_Create.pack(side=LEFT)
 
-        self.button_Cancel = Button(self.myContainer1, text="Cancel", bg="red")
+        self.button_Cancel = Button(self.frm_container_01, text="Cancel", bg="red")
         self.button_Cancel.bind("<Button-1>", self.buttonCloseClick)
         self.button_Cancel.pack(side=RIGHT)
 
@@ -115,7 +109,8 @@ class UpdateTagWindow(Toplevel):
         if self.update_status == "Tag updated.":
             self.myDict = self.newDict
             AlertWindow(self, "Success", "Tag was updated.")
-            return
+            self.destroy()
+            return MyApp.show_content(myapp)
 
         AlertWindow(self, "Error", self.update_status)
         return
@@ -123,6 +118,7 @@ class UpdateTagWindow(Toplevel):
     def buttonCloseClick(self, event):
         report_event(event)
         self.destroy()
+        MyApp.show_content()
 
 
 class NewTagWindow(Toplevel):
@@ -144,14 +140,14 @@ class NewTagWindow(Toplevel):
         self.text_Switch_to = Text(self)
         self.text_Switch_to.pack(fill=X)
 
-        self.myContainer1 = Frame(self)
-        self.myContainer1.pack()
+        self.frm_container_01 = Frame(self)
+        self.frm_container_01.pack()
 
-        self.button_Create = Button(self.myContainer1, text="Create", bg="green")
+        self.button_Create = Button(self.frm_container_01, text="Create", bg="green")
         self.button_Create.bind("<Button-1>", self.buttonCreateClick)
         self.button_Create.pack(side=LEFT)
 
-        self.button_Cancel = Button(self.myContainer1, text="Cancel", bg="red")
+        self.button_Cancel = Button(self.frm_container_01, text="Cancel", bg="red")
         self.button_Cancel.bind("<Button-1>", self.buttonCancelClick)
         self.button_Cancel.pack(side=RIGHT)
 
